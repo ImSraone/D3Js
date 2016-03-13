dea1d2247a723dbcd53113bd807ffd62d5d0bbec
Sravan Vanteru <sravan.vanteru@gmail.com>
2016-03-13 14:55:50 +0530

    first commit with all changes

---
+++ D3JsDemo/Controllers/ChartsController.cs
@@ -0,0 +1,57 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Script.Serialization;
+
+namespace PowerBI.Controllers
+{
+    public class ChartsController : Controller
+    {
+        [OutputCache(Duration = 100)]
+        public ActionResult AllCharts()
+        {
+            // for creating new Dataset in Power BI
+            //var response = createDataset(Session["AccessToken"].ToString());
+            return View();
+        }
+        public ActionResult pieChart()
+        {
+            return View();
+        }
+        public ActionResult stackedColumnChart()
+        {
+            return View();
+        }
+        public ActionResult collapsibleTreeChart()
+        {
+            return View();
+        }
+        public ActionResult areaChart()
+        {
+            return View();
+        }
+        public ActionResult simpleBarChart()
+        {
+            return View();
+        }
+        public ActionResult lineChart()
+        {
+            return View();
+        }
+        public ActionResult GeoNamesExtract()
+        {
+            return View();
+        }
+        public ActionResult DraggableNodes()
+        {
+            return View();
+        }
+    }
+}
---
+++ D3JsDemo/Controllers/PracticeController.cs
@@ -0,0 +1,111 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Script.Serialization;
+
+namespace PowerBI.Controllers
+{
+    public class PracticeController : Controller
+    {
+        [OutputCache(Duration=100)]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult basicSVGShapes()
+        {
+            return View();
+        }
+        public ActionResult visualizingData()
+        {
+            return View();
+        }
+        public ActionResult GroupsAndAxes()
+        {
+            return View();
+        }
+        public ActionResult EnterUpdateExit()
+        {
+            return View();
+        }
+        public ActionResult Transitions()
+        {
+            return View();
+        }
+        public ActionResult WorkingWithArrays()
+        {
+            return View();
+        }
+        public ActionResult LoadingExternalData()
+        {
+            return View();
+        }
+        public ActionResult Paths()
+        {
+            return View();
+        }
+        public ActionResult Arcs()
+        {
+            return View();
+        }
+        public ActionResult PieLayout()
+        {
+            return View();
+        }
+        public ActionResult TreeLayout()
+        {
+            return View();
+        }
+        public ActionResult TreeLayout2()
+        {
+            return View();
+        }
+        public ActionResult ClusterPack()
+        {
+            return View();
+        }
+        public ActionResult Bubble()
+        {
+            return View();
+        }
+        public ActionResult HistogramLayout()
+        {
+            return View();
+        }
+        public ActionResult TreeMapLayout()
+        {
+            return View();
+        }
+        public ActionResult Maps()
+        {
+            return View();
+        }
+        public ActionResult colorLineChart()
+        {
+            return View();
+        }
+        public ActionResult BarChart()
+        {
+            return View();
+        }
+        public ActionResult PieChart()
+        {
+            return View();
+        }
+        public ActionResult Gauge()
+        {
+            return View();
+        }
+        public ActionResult SVGExample()
+        {
+            return View();
+        }
+    }
+}
